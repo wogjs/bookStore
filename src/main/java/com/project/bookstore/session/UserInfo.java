c44b78f83c3b3bcf0030e2f29ba0266f55f854d3
hy1152 <wogjs1152@gmail.com>
2020-11-04 22:21:36 +0900

    마이그레이션 및 Session 완료

---
+++ src/main/java/com/project/bookstore/session/UserInfo.java
@@ -0,0 +1,20 @@
+package com.project.bookstore.session;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Component
+@Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+@ToString
+public class UserInfo implements Serializable {
+    private String userId;
+}
