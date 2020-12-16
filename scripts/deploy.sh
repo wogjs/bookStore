772f72bcea651b79bc079489ef00fc7ed671abbd
wogjs <50190255+wogjs@users.noreply.github.com>
2020-12-16 22:10:44 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,7 +11,9 @@ echo "> 현재 구동 중인 애플리케이션 pid확인"
 
 CURRENT_PID=$(pgrep -fl springboot-bookstore | grep jar | awk '{print $1}')
 
-if [ -z ""CURRENT_PID" ]; then
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
 	echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
 	echo "> kill -15 $CURRENT_PID"
@@ -23,9 +25,9 @@ echo "> 새 애플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR Name : $JAR_NAME"
+echo "> JAR Name : $JAR_NAME "
 
-echo "> JAR_NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
